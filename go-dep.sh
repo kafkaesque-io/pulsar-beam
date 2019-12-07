170205f3a0725ae9a8c069d3c5e270573dcf5895
ming luo <itestmycode@gmail.com>
2019-12-07 14:17:18 -0500

    add script to perform go get for CI

---
+++ go-dep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#
+# a script to install go dependencies
+# used for CI (i.e. GitHub actions)
+#
+go get -u github.com/gorilla/mux
+go get -u github.com/rs/cors
+go get -u github.com/patrickmn/go-cache
