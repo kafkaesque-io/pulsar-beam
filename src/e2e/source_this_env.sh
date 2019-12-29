abd0c018c6d0ab559213508e0c77d0aa2eaf9802
Ming <itestmycode@gmail.com>
2019-12-28 21:16:56 -0500

    add e2e test tool (#15)

---
+++ src/e2e/source_this_env.sh
@@ -0,0 +1,10 @@
+#
+# These env should be provided by CI runtime env variable.
+# Only source this file for local testing purpose.
+#
+export PULSAR_TOKEN="eyJhbG"
+export PULSAR_URI="pulsar+ssl://useast1.gcp.kafkaesque.io:6651"
+export WEBHOOK_TOPIC="persistent://"
+export REST_API_TOKEN="eyJhbG"
+export WEBHOOK2_URL="http://localhost:8080/wh"
+export FN_SINK_TOPIC="persistent://"
