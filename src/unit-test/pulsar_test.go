7211a57c7f7fcfe927918e6cfcbd64b130889f50
Ming Luo <itestmycode@gmail.com>
2020-02-23 20:58:38 -0500

    enable codecov.yml

---
+++ src/unit-test/pulsar_test.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"os"
+	"testing"
+
+	"github.com/apache/pulsar/pulsar-client-go/pulsar"
+	"github.com/pulsar-beam/src/pulsardriver"
+	"github.com/pulsar-beam/src/util"
+)
+
+func TestClientCreation(t *testing.T) {
+	util.Config.DbConnectionStr = os.Getenv("PULSAR_URI")
+	util.Config.DbName = os.Getenv("REST_DB_TABLE_TOPIC")
+	util.Config.DbPassword = os.Getenv("PULSAR_TOKEN")
+	util.Config.PbDbType = "pulsarAsDb"
+	util.Config.TrustStore = os.Getenv("TrustStore")
+
+	_, _ = pulsardriver.GetTopicDriver("pulsar://test url", "token")
+	//errNil(t, err)
+	//assert(t, err == nil, "create pulsar driver with bogus url")
+
+	_, _ = pulsardriver.GetConsumer("pulsar://test url", "token", "topicname", "sub", "subKey", pulsar.Failover, pulsar.Latest)
+	//assert(t, err != nil, "create pulsar consumer with bogus url")
+}
