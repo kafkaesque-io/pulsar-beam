bd9e015be6c6e00a42edf9715ab62da92905f821
Ming Luo <itestmycode@gmail.com>
2020-06-05 11:29:57 -0400

    remove unused SSE notification channel

--- src/broker/sse-broker.go
+++ src/broker/sse-broker.go
@@ -5,27 +5,11 @@ import (
 	"github.com/kafkaesque-io/pulsar-beam/src/pulsardriver"
 )
 
-// SSEBroker is an SSE (Sever Sent Event) Broker holds open client connections,
-// listens for incoming events on its Notifier channel
-// and send message to all registered connections
-type SSEBroker struct {
-
-	// Events are pushed to this channel by the main events-gathering routine
-	Notifier chan []byte
-
-	// New client connections
-	newClients chan chan []byte
-
-	// Closed client connections
-	closingClients chan chan []byte
-
-	// Client connections registry
-	clients map[chan []byte]bool
-}
-
 const (
 	// SSEBrokerMaxSize the max size of the number of HTTP SSE session is supported
 	SSEBrokerMaxSize = 200
+
+	// TODO add counters and max limit for SSEBroker
 )
 
 // GetPulsarClientConsumer returns Puslar client and consumer interface objects
